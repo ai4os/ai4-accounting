845d9f24d58750861823baf7f79563331de4f411
Ignacio Heredia <iheredia@ifca.unican.es>
2024-05-08 12:47:38 +0200

    fix: change Nomad cert paths

--- take_snapshot.sh
+++ take_snapshot.sh
@@ -6,9 +6,9 @@
 
 # Export proper Nomad variables
 export NOMAD_ADDR=https://193.146.75.205:4646  # production cluster
-export NOMAD_CACERT=/home/ubuntu/nomad-certs/nomad-prod/nomad-ca.pem
-export NOMAD_CLIENT_CERT=/home/ubuntu/nomad-certs/nomad-prod/cli.pem
-export NOMAD_CLIENT_KEY=/home/ubuntu/nomad-certs/nomad-prod/cli-key.pem
+export NOMAD_CACERT=/home/ubuntu/nomad-certs/nomad-federated/nomad-ca.pem
+export NOMAD_CLIENT_CERT=/home/ubuntu/nomad-certs/nomad-federated/cli.pem
+export NOMAD_CLIENT_KEY=/home/ubuntu/nomad-certs/nomad-federated/cli-key.pem
 export NOMAD_TLS_SERVER_NAME=node-ifca-0
 
 # Move to main directory (where this script is located)
